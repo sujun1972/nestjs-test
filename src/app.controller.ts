291a9630fe260fab0e2d49fd09aacb4c1e8b16f9
Jun <sujun1972@gmail.com>
2020-02-17 10:52:56 +0100

    20200217

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Headers, Ip, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IpAddress } from './ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(@Headers() headers: any, @Ip() ip: any): string {
-    Logger.log(ip);
+  getHello(@Headers() headers: any, @Ip() ip: any, @IpAddress() ipAddress: any): string {
+    Logger.log(ipAddress);
     Logger.log('========----------------------========');
     Logger.log(headers);
     return this.appService.getHello();
